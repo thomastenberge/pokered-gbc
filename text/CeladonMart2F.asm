9b69bca65436fe136903210118e82f24146c110b
thomastenberge <thomastenberge.git@gmail.com>
2024-06-18 02:36:04 +0200

    - Clarified the text for the super potion on 2F of the celadon dept store

--- text/CeladonMart2F.asm
+++ text/CeladonMart2F.asm
@@ -1,10 +1,10 @@
 _CeladonMart2FMiddleAgedManText::
 	text "SUPER REPEL keeps"
 	line "weak #MON at"
-	cont "bay..."
+	cont "bay for longer..."
 
 	para "Hmm, it's a more"
-	line "powerful REPEL!"
+	line "potent REPEL!"
 	done
 
 _CeladonMart2FGirlText::
