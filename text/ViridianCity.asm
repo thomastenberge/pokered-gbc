9758816a050ac3a8400466752ee04197cfa4e13b
thomastenberge <thomastenberge.git@gmail.com>
2024-06-22 23:07:09 +0200

    Make town signs a bit better scripted

--- text/ViridianCity.asm
+++ text/ViridianCity.asm
@@ -150,8 +150,8 @@ _ViridianCityOldManYouNeedToWeakenTheTargetText::
 
 _ViridianCitySignText::
 	text "VIRIDIAN CITY"
-	line "The Eternally"
-	cont "Green Paradise"
+	para "The Eternally"
+	line "Green Paradise"
 	done
 
 _ViridianCityTrainerTips1Text::
